28ab9d91ee71f6f6cd7e2cfb4cabbdf27544bbaa
Hugo MG Sung <personar95@naver.com>
2026-06-08 17:07:15 +0900

    데스크톱 개발 1일차

---
+++ wpf/WpfPracticeSolution/WpfCafeKiosk/Models/OrderItem.cs
@@ -0,0 +1,19 @@
+﻿namespace WpfCafeKiosk.Models
+{
+    public class OrderItem
+    {
+        // menu_id, menu_name, price, image_path, cateogry, is_sale DB 컬럼명
+        // MenuId, MenuNane, Price, ImagePath, Category, IsSale 클래스 속성명
+        public int MenuId { get; set; }
+        public string MenuName { get; set; }
+        public int Price { get; set; }
+        public int Count { get; set; }
+        
+        public int TotalPrice { 
+            get 
+            {
+                return Price * Count;
+            } 
+        }
+    }
+}
